0c8323202d920f4201dedcc9cb9f77ad2e0b5c01
mwx@rmbp <mwx36mwx@gmail.com>
2017-04-28 15:42:24 +0800

    initial scaffold

---
+++ src/robot/dmotion/src/main.cpp
@@ -0,0 +1,11 @@
+#include <ros/ros.h>
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "dmotion_node");
+  ros::NodeHandle n;
+  ros::Rate r(100);
+  while(ros::ok()) {
+    ROS_INFO("Hi ROS");
+    r.sleep();
+  }
+}
