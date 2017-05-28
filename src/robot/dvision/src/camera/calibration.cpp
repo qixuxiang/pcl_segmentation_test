ded29e2efc7404dca16808ee798d117c39bdeae4
mwx@zz <mwx36mwx@163.com>
2017-05-28 20:01:01 +0800

    simplify calibration

--- src/robot/dvision/src/camera/calibration.cpp
+++ src/robot/dvision/src/camera/calibration.cpp
@@ -7,74 +7,36 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
+#define BOARDWIDTH 9
+#define BOARDHEIGHT 9
+#define SQUARESIZE 30
+
 using namespace std;
 using namespace cv;
 
-class CalibSettings
+struct CalibSettings
 {
-  public:
-    enum Pattern
-    {
-        NOT_EXISTING,
-        CHESSBOARD,
-        CIRCLES_GRID,
-        ASYMMETRIC_CIRCLES_GRID
-    };
-
-    CalibSettings()
-      : calibMode(CHESSBOARD)
-    {
-    }
-
-    void init(int boardWidth, int boardHeight, float squareSize)
-    {
-        this->boardWidth = boardWidth;
-        this->boardHeight = boardHeight;
-        this->squareSize = squareSize;
-    }
-
     int getFlag()
     {
         int flag = 0;
-        // flag |= CV_CALIB_USE_INTRINSIC_GUESS; // keep camera matrix as zero
-        // flag |= CV_CALIB_FIX_ASPECT_RATIO;
-        // flag |= CV_CALIB_FIX_PRINCIPAL_POINT;
-        // flag |= CV_CALIB_ZERO_TANGENT_DIST;
-        // flag |= CV_CALIB_FIX_FOCAL_LENGTH;
-        // flag |= CV_CALIB_FIX_K1;
-        // flag |= CV_CALIB_FIX_K2;
-        // flag |= CV_CALIB_FIX_K3;
-        // if (!params.calib->high_dimension()) {
-        //   flag |= CV_CALIB_FIX_K4;
-        //   flag |= CV_CALIB_FIX_K5;
-        //   flag |= CV_CALIB_FIX_K6;
-        // }
         flag |= CV_CALIB_RATIONAL_MODEL;
         flag |= CV_CALIB_THIN_PRISM_MODEL;
+        flag |= CALIB_TILTED_MODEL;
         return flag;
     }
 
     Size getBoardSize()
     {
-        return Size(boardWidth, boardHeight);
+        return Size(BOARDWIDTH, BOARDHEIGHT);
     }
 
     float getSquareSize()
     {
-        return squareSize;
+        return SQUARESIZE;
     }
-
-    Pattern getCalibMode()
-    {
-        return calibMode;
-    };
-
-  private:
-    int boardWidth = 9;
-    int boardHeight = 9;
-    float squareSize = 30;
-    Pattern calibMode;
 };
+
+CalibSettings s;
 
 static double
 computeReprojectionErrors(const vector<vector<Point3f>>& objectPoints,
@@ -104,140 +66,113 @@ computeReprojectionErrors(const vector<vector<Point3f>>& objectPoints,
 }
 
 static void
-calcBoardCornerPositions(Size boardSize, float squareSize, vector<Point3f>& corners, CalibSettings::Pattern patternType)
+calcBoardCornerPositions(Size boardSize, float squareSize, vector<Point3f>& corners)
 {
     corners.clear();
-    for (int i = 0; i < boardSize.height; ++i) {
-        for (int j = 0; j < boardSize.width; ++j) {
+    for (int i = 0; i < boardSize.height; ++i)
+        for (int j = 0; j < boardSize.width; ++j)
             corners.push_back(Point3f(j * squareSize, i * squareSize, 0));
-        }
-    }
 }
 
 static bool
-runCalibration(CalibSettings& s,
-               Size& imageSize,
-               Mat& cameraMatrix,
-               Mat& distCoeffs,
-               vector<vector<Point2f>> imagePoints,
-               vector<Mat>& rvecs,
-               vector<Mat>& tvecs,
-               vector<float>& reprojErrs,
-               double& totalAvgErr)
+runCalibration(Size& imageSize, Mat& cameraMatrix, Mat& distCoeffs, vector<vector<Point2f>> imagePoints, vector<Mat>& rvecs, vector<Mat>& tvecs, vector<float>& reprojErrs, double& totalAvgErr)
 {
     cameraMatrix = Mat::eye(3, 3, CV_64F);
-    if (s.getFlag() & CALIB_FIX_ASPECT_RATIO)
+    if (s.getFlag() & CALIB_FIX_ASPECT_RATIO) {
         cameraMatrix.at<double>(0, 0) = 1.0;
-
+    }
     distCoeffs = Mat::zeros(8, 1, CV_64F);
-
     vector<vector<Point3f>> objectPoints(1);
-    calcBoardCornerPositions(s.getBoardSize(), s.getSquareSize(), objectPoints[0], s.getCalibMode());
-
+    calcBoardCornerPositions(s.getBoardSize(), s.getSquareSize(), objectPoints[0]);
     objectPoints.resize(imagePoints.size(), objectPoints[0]);
-
     // Find intrinsic and extrinsic camera parameters
     double rms = calibrateCamera(objectPoints, imagePoints, imageSize, cameraMatrix, distCoeffs, rvecs, tvecs, s.getFlag());
-
     cout << "Re-projection error reported by calibrateCamera: " << rms << endl;
-
     bool ok = checkRange(cameraMatrix) && checkRange(distCoeffs);
-
     totalAvgErr = computeReprojectionErrors(objectPoints, imagePoints, rvecs, tvecs, cameraMatrix, distCoeffs, reprojErrs);
-
     return ok;
 }
 
 bool
-runCalibrationAndSave(CalibSettings& s, Size imageSize, Mat& cameraMatrix, Mat& distCoeffs, vector<vector<Point2f>> imagePoints)
+runCalibrationAndSave(Size imageSize, Mat& cameraMatrix, Mat& distCoeffs, vector<vector<Point2f>> imagePoints)
 {
     vector<Mat> rvecs, tvecs;
     vector<float> reprojErrs;
     double totalAvgErr = 0;
-
-    bool ok = runCalibration(s, imageSize, cameraMatrix, distCoeffs, imagePoints, rvecs, tvecs, reprojErrs, totalAvgErr);
+    bool ok = runCalibration(imageSize, cameraMatrix, distCoeffs, imagePoints, rvecs, tvecs, reprojErrs, totalAvgErr);
     cout << (ok ? "Calibration succeeded" : "Calibration failed") << ". avg re projection error = " << totalAvgErr << endl;
-
     return ok;
 }
 
-int
-main()
+vector<string>
+getImageList(string path)
 {
-    CalibSettings s;
-    vector<vector<Point2f>> imagePoints;
-    Mat cameraMatrix, distCoeffs;
-    Size imageSize;
-
-    int imagesNum = 0;
-    int foundCornerNum = 0;
-
     vector<string> imagesName;
-    // TODO edit calibrating image's path
-    string pathDirectory = "/home/mwx/Pictures/calibration/";
     DIR* dir;
     struct dirent* ent;
-    if ((dir = opendir(pathDirectory.c_str())) != NULL) {
-        /* print all the files and directories within directory */
+    if ((dir = opendir(path.c_str())) != NULL) {
         while ((ent = readdir(dir)) != NULL) {
             string tmpFileName = ent->d_name;
             if (tmpFileName.length() > 4) {
                 auto nPos = tmpFileName.find(".png");
                 if (nPos != string::npos) {
-                    imagesName.push_back(tmpFileName);
-                    imagesNum++;
+                    imagesName.push_back(path + '/' + tmpFileName);
                 }
             }
         }
         closedir(dir);
-    } else {
-        perror("");
-        return EXIT_FAILURE;
+    }
+    return imagesName;
+}
+
+int
+main(int argc, char** argv)
+{
+    if (argc != 2) {
+        cout << "Usage: " << argv[0] << " <pic path>" << endl;
+        return 0;
     }
 
-    sort(imagesName.begin(), imagesName.end());
+    string pathDirectory = argv[1];
+    auto imagesName = getImageList(pathDirectory);
 
+    vector<vector<Point2f>> imagePoints;
+    Mat cameraMatrix, distCoeffs;
+    Size imageSize;
     for (auto image_name : imagesName) {
         Mat view;
-        view = imread((pathDirectory + image_name).c_str());
+        view = imread(image_name.c_str());
 
         imageSize = view.size();
         vector<Point2f> pointBuf;
-        bool found;
-
-        found = findChessboardCorners(view, s.getBoardSize(), pointBuf, CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_FAST_CHECK | CALIB_CB_NORMALIZE_IMAGE);
+        bool found = findChessboardCorners(view, s.getBoardSize(), pointBuf, CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_FAST_CHECK | CALIB_CB_NORMALIZE_IMAGE);
         if (found) {
             Mat viewGray;
             cvtColor(view, viewGray, COLOR_BGR2GRAY);
             cornerSubPix(viewGray, pointBuf, Size(11, 11), Size(-1, -1), TermCriteria(TermCriteria::EPS + TermCriteria::COUNT, 30, 0.1));
-
             imagePoints.push_back(pointBuf);
             drawChessboardCorners(view, s.getBoardSize(), Mat(pointBuf), found);
-            foundCornerNum++;
-
             namedWindow("image", CV_WINDOW_NORMAL);
             imshow("image", view);
-            waitKey(0);
-
-            cout << image_name << " found corner successfully!" << endl;
+            cvWaitKey(0);
         } else {
             cout << image_name << " found corner failed! & removed!" << endl;
-            //      remove((pathDirectory + image_name).c_str());
+            remove(image_name.c_str());
         }
     }
 
-    cout << foundCornerNum << " of " << imagesNum << " found corner successfully !" << endl;
-    runCalibrationAndSave(s, imageSize, cameraMatrix, distCoeffs, imagePoints);
-    cout << "-------------cameraMatrix--------------" << endl;
+    runCalibrationAndSave(imageSize, cameraMatrix, distCoeffs, imagePoints);
 
+    cout << "-------------cameraMatrix--------------" << endl;
     cout << cameraMatrix.size() << endl;
     cout << cameraMatrix << endl;
+
     cout << "---------------distCoeffs--------------" << endl;
     cout << distCoeffs.size() << endl;
     cout << distCoeffs << endl;
 
     for (auto image_name : imagesName) {
-        Mat view = imread((pathDirectory + image_name).c_str());
+        Mat view = imread(image_name.c_str());
         Mat temp = view.clone();
         undistort(temp, view, cameraMatrix, distCoeffs);
         cout << image_name << endl;
