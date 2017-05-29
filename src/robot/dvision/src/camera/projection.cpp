6811aa350888894477ddd88c995d3acc3e60478c
mwx@zz <mwx36mwx@163.com>
2017-05-29 23:03:15 +0800

    add projection

--- src/robot/dvision/src/camera/projection.cpp
+++ src/robot/dvision/src/camera/projection.cpp
@@ -1,13 +1,34 @@
 #include "dvision/projection.hpp"
 
 namespace dvision {
-Projection::init()
+void Projection::init()
 {
     //
 }
 
-Projection::update() {
-
+void Projection::update()
+{
+    // update camera location and orientation
+    // calculate homography
 }
 
+void
+Projection::getOnImageCoordinate(const cv::Point& point, cv::Point2f& resPoint)
+{
+}
+
+void
+Projection::getOnImageCoordinate(const std::vector<cv::Point>& points, std::vector<cv::Point2f>& resPoints)
+{
+}
+
+void
+Projection::getOnRealCoordinate(const cv::Point2f& point, cv::Point& resPoint)
+{
+}
+
+void
+Projection::getOnRealCoordinate(const std::vector<cv::Point2f>& points, std::vector<cv::Point>& resPoints)
+{
+}
 } // namespace dvision
