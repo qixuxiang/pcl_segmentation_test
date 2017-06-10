2070f9c39fda58835771ae64c25997205a1b7252
corenel <xxdsox@gmail.com>
2017-06-10 13:54:17 +0800

    init darknetcxx package

---
+++ src/robot/dlib/darknetcxx/include/darknetcxx/detector.hpp
@@ -0,0 +1,10 @@
+/**
+ * @Author: Yusu Pan <yuthon>
+ * @Date:   2017-06-10T13:44:14+08:00
+ * @Email:  xxdsox@gmail.com
+ * @Project: humanoid
+ * @Filename: detector.hpp
+ * @Last modified by:   yuthon
+ * @Last modified time: 2017-06-10T13:44:15+08:00
+ * @Copyright: ZJUDancer
+ */
---
+++ src/robot/dlib/darknetcxx/src/detector.cpp
@@ -0,0 +1,12 @@
+/**
+ * @Author: Yusu Pan <yuthon>
+ * @Date:   2017-06-10T13:44:28+08:00
+ * @Email:  xxdsox@gmail.com
+ * @Project: humanoid
+ * @Filename: detector.cpp
+ * @Last modified by:   yuthon
+ * @Last modified time: 2017-06-10T13:44:29+08:00
+ * @Copyright: ZJUDancer
+ */
+
+#include "darknetcxx/detector.hpp"
