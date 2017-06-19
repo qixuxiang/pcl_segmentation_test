887ed9fac4e12e62755c4da85520c6bd3c9ff330
corenel <xxdsox@gmail.com>
2017-06-19 14:39:00 +0800

    fix localization, now loc optimization works.
    field model parameters should be init in Init() instead of constructor.

--- src/robot/dvision/src/dvision.cpp
+++ src/robot/dvision/src/dvision.cpp
@@ -29,11 +29,9 @@ DVision::DVision(ros::NodeHandle* n)
     m_pub = m_nh->advertise<VisionInfo>("/humanoid/VisionInfo", 1);
 
     m_transmitter = new dtransmit::DTransmit(parameters.udpBroadcastAddress);
-    if(parameters.simulation) {
+    if (parameters.simulation) {
         ROS_INFO("Simulation mode!");
-        m_transmitter->addRosRecv<VisionInfo>(dconstant::network::monitorBroadcastAddressBase + parameters.robotId, [&](VisionInfo& msg){
-            m_data = msg;
-        });
+        m_transmitter->addRosRecv<VisionInfo>(dconstant::network::monitorBroadcastAddressBase + parameters.robotId, [&](VisionInfo& msg) { m_data = msg; });
     }
 }
 
@@ -48,7 +46,7 @@ DVision::tick()
     /**********
      * Update *
      **********/
-    if(parameters.simulation) {
+    if (parameters.simulation) {
         m_pub.publish(m_data);
         return;
     }
@@ -94,15 +92,14 @@ DVision::tick()
         m_data.see_circle = m_circle.Process(m_line.clustered_lines());
     }
 
-    if (m_data.see_circle){
-      if(m_ball_tracker.Process(m_circle.result_circle().x, m_circle.result_circle().y, static_cast<double>(m_pitch), static_cast<double>(m_yaw))){
-        m_data.cmd_head_ball_track.x = 0;
-        m_data.cmd_head_ball_track.y = m_ball_tracker.m_out_pitch / M_PI * 180;
-        m_data.cmd_head_ball_track.z = m_ball_tracker.m_out_yaw / M_PI * 180;
-        // cout << "c_pitch: " << m_data.cmd_head_ball_track.y << endl;
-        // cout << "c_yaw: " << m_data.cmd_head_ball_track.z << endl;
-      }
-
+    if (m_data.see_circle) {
+        if (m_ball_tracker.Process(m_circle.result_circle().x, m_circle.result_circle().y, static_cast<double>(m_pitch), static_cast<double>(m_yaw))) {
+            m_data.cmd_head_ball_track.x = 0;
+            m_data.cmd_head_ball_track.y = m_ball_tracker.m_out_pitch / M_PI * 180;
+            m_data.cmd_head_ball_track.z = m_ball_tracker.m_out_yaw / M_PI * 180;
+            // cout << "c_pitch: " << m_data.cmd_head_ball_track.y << endl;
+            // cout << "c_yaw: " << m_data.cmd_head_ball_track.z << endl;
+        }
     }
     /*****************
      * Goal Detector *
@@ -211,18 +208,19 @@ DVision::prepareVisionInfo(VisionInfo& m_data)
     m_data.circle_field.x = result_circle_rotated.x;
     m_data.circle_field.y = result_circle_rotated.y;
 
-    auto& lines = m_line.clustered_lines();
-    m_data.lines.resize(lines.size());
-    for(uint32_t i = 0; i < lines.size(); ++i) {
-        auto& p1 = lines[i].P1;
+    std::vector<LineSegment> lines_global(m_line.clustered_lines().size());
+    getOnGlobalCoordinate(m_loc.location(), m_line.clustered_lines(), lines_global);
+    m_data.lines.resize(lines_global.size());
+    for (uint32_t i = 0; i < lines_global.size(); ++i) {
+        auto& p1 = lines_global[i].P1;
         m_data.lines[i].endpoint1.x = p1.x;
         m_data.lines[i].endpoint1.y = p1.y;
 
-        auto& p2 = lines[i].P2;
+        auto& p2 = lines_global[i].P2;
         m_data.lines[i].endpoint2.x = p2.x;
         m_data.lines[i].endpoint2.y = p2.y;
     }
-//    m_line.clustered_lines()
+    //    m_line.clustered_lines()
     // ball
     // if (m_data.loc_ok) {
     //     // TODO(corenel) Rotate angle is correct?
