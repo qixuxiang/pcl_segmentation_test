751ccf73d0673f7348585c6cfd4761c7734f503c
corenel <xxdsox@gmail.com>
2017-06-19 19:33:42 +0800

    reinit behaviour request in tick.

--- src/robot/dbehavior/scripts/misc/types/behaviour_request.py
+++ src/robot/dbehavior/scripts/misc/types/behaviour_request.py
@@ -11,7 +11,7 @@
 import rospy
 from geometry_msgs.msg import Vector3
 from dmotion.msg import ActionCmd
-from dbehavior.msg import BehaviourShareData, TeamShareData
+from dbehavior.msg import BehaviourInfo, TeamInfo
 from ..types.constant import UNKNOWN
 from ..types.action_command import crouch, head
 
@@ -22,13 +22,29 @@ class BehaviourRequest(object):
     def __init__(self):
         """Init."""
         super(BehaviourRequest, self).__init__()
+        self.reinit()
+        # publish node
+        self.actions_pub = rospy.Publisher('/humanoid/ActionCommand',
+                                           ActionCmd, queue_size=1)
+        self.behaviour_pub = rospy.Publisher('/humanoid/BehaviourInfo',
+                                             BehaviourInfo, queue_size=1)
+        self.team_pub = rospy.Publisher('/humanoid/TeamInfo',
+                                        TeamInfo, queue_size=1)
+
+    def publish(self):
+        """Publish messages."""
+        self.actions_pub.publish(self.actions)
+        self.behaviour_pub.publish(self.behaviour)
+        self.team_pub.publish(self.team)
+
+    def reinit(self):
+        """Reinit self with default value."""
         # action command
-        # self.update_gait_vec = False
         self.actions = ActionCmd()
         crouch(self.actions)
         head(self.actions, 0, 0)
         # behaviour request
-        self.behaviour = BehaviourShareData()
+        self.behaviour = BehaviourInfo()
         self.behaviour.enable_localization = False
         self.behaviour.destination = Vector3(UNKNOWN, UNKNOWN, 0)
         self.behaviour.kick = False
@@ -36,23 +52,10 @@ class BehaviourRequest(object):
         self.behaviour.reset_localization = False
         self.behaviour.reset_point = Vector3(UNKNOWN, UNKNOWN, 0)
         # team
-        self.team = TeamShareData()
+        self.team = TeamInfo()
         self.team.goalie_attacking = False
         self.team.time_to_reach_ball = 9999.0
         self.team.time_to_reach_striker = 9999.0
         self.team.time_to_reach_midfielder = 9999.0
         self.team.time_to_reach_defender = 9999.0
         self.team.current_role = None
-        # publish node
-        self.actions_pub = rospy.Publisher('/humanoid/ActionCommand',
-                                           ActionCmd, queue_size=1)
-        self.behaviour_pub = rospy.Publisher('/humanoid/BehaviourShareData',
-                                             BehaviourShareData, queue_size=1)
-        self.team_pub = rospy.Publisher('/humanoid/TeamShareData',
-                                        TeamShareData, queue_size=1)
-
-    def publish(self):
-        """Publish messages."""
-        self.actions_pub.publish(self.actions)
-        self.behaviour_pub.publish(self.behaviour)
-        self.team_pub.publish(self.team)
