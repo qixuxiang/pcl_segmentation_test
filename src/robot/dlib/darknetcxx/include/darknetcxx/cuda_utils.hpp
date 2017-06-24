ff027641a98b188eb0005d50de6f5e6eb95428f5
corenel <xxdsox@gmail.com>
2017-06-24 16:03:45 +0800

    output Ptr_gpu ctor and resize message for debug.

--- src/robot/dlib/darknetcxx/include/darknetcxx/cuda_utils.hpp
+++ src/robot/dlib/darknetcxx/include/darknetcxx/cuda_utils.hpp
@@ -109,11 +109,13 @@ class Ptr_gpu : NonCopyable
       : m_size(0)
       , m_ptr(nullptr, deleter())
     {
+        std::cout << "Ptr_gpu ctor" << std::endl;
     }
     Ptr_gpu(int size)
       : m_size(size)
       , m_ptr(cuda_make_array<T>(nullptr, size), deleter())
     {
+        std::cout << "Ptr_gpu ctor" << std::endl;
     }
 
     T* get()
@@ -128,6 +130,7 @@ class Ptr_gpu : NonCopyable
         m_allocated += sizeof(T) * size / 1000000.;
         printf("GPU resized from %lu to %lu bytes, all %lf MB\n", sizeof(T) * m_size, sizeof(T) * size, m_allocated);
 #endif
+        std::cout << "Ptr gpu resized to " << size << std::endl;
         if (m_ptr.get() != nullptr) {
             cuda_free(m_ptr.release());
         }
